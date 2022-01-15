1d32878c253863deab15d596c59122e0125f2483
Kivanc Tatar <kivanctatar@pm.me>
2022-01-15 19:11:19 +0100

    cloud run setups

--- jb-ltsp-1.sh
+++ jb-ltsp-1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1        # request GPU "generic resource"
+#SBATCH --gres=gpu:v100l:1        # request GPU "generic resource"
 #SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=72000M        # memory per node
 #SBATCH --time=10-22:00      # time (DD-HH:MM)
