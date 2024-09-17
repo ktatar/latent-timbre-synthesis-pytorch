a1e56f72fe6d0610db70d416f9659ecf81a0e035
Kivanc Tatar <bucket@kivanctatar.com>
2024-09-17 14:19:27 +0000

    Sep 17, 2024, 4:19 PM

---
+++ jb-ltsp-alvis-1.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#SBATCH -A NAISS2024-22-16 -p alvis
+#SBATCH -t 0-10:00:00
+#SBATCH --gpus-per-node=A40:1
+
+module load virtualenv/20.23.1-GCCcore-12.3.0 PyTorch/2.1.2-foss-2023a-CUDA-12.1.1 SciPy-bundle/2023.07-gfbf-2023a matplotlib/3.7.2-gfbf-2023a  
+sleep 5
+source /cephyr/users/tatar/Alvis/ltsp/bin/activate
+
+cd /cephyr/users/tatar/Alvis/code/latent-timbre-synthesis-pytorch
+
+python create_dataset.py --config ./default-alvis.ini
+
+# python ./train.py --config ./default-alvis.ini
