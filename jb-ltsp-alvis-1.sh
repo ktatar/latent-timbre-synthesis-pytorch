a3fe47a667371b07aa423273a26fe72df052bbb0
Kivanc Tatar <bucket@kivanctatar.com>
2024-09-26 19:31:12 +0000

    Sep 26, 2024, 9:31 PM

--- jb-ltsp-alvis-1.sh
+++ jb-ltsp-alvis-1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH -A NAISS2024-22-16 -p alvis
-#SBATCH -t 7-00:00:00
+#SBATCH -t 5-00:00:00
 #SBATCH --gpus-per-node=A100:1
 #SBATCH --mail-user=tatar@chalmers.se
 #SBATCH --mail-type=BEGIN
